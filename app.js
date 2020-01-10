5fe0357b7e9a068e17ea492aaa3cf29e3e2e3fac
Behnam Ebrahimi <behnam.ebrahimi@gmail.com>
2020-01-10 16:46:01 +1100

    -

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ if (process.env.NODE_ENV === 'production') {
 
   // express will serve up the index.html file
   // if it doesn't recognize the route
-  app.get('*', (req, res) => {
+  app.get('/*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
--- client/src/actions/surveyActions.js
+++ client/src/actions/surveyActions.js
@@ -2,7 +2,7 @@ import backend from '../apis/backend';
 import { FETCH_USER, FETCH_SURVEYS } from './types';
 
 export const submitSurvey = (values, history) => async dispatch => {
-  const { data: user } = await backend.post('/surveys', values);
+  const { data: user } = await backend.post('/api/surveys', values);
 
   dispatch({ type: FETCH_USER, payload: user });
 
@@ -10,7 +10,7 @@ export const submitSurvey = (values, history) => async dispatch => {
 };
 
 export const fetchSurveys = () => async dispatch => {
-  const { data: surveys } = await backend.get('/surveys');
+  const { data: surveys } = await backend.get('/api/surveys');
 
   dispatch({ type: FETCH_SURVEYS, payload: surveys });
 };
