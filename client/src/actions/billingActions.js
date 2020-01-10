81919800e774b63166c6e8ad73b5a9f31e0f556d
Behnam Ebrahimi <behnam.ebrahimi@gmail.com>
2020-01-10 16:12:24 +1100

    fixed another bug

--- client/src/actions/billingActions.js
+++ client/src/actions/billingActions.js
@@ -2,7 +2,7 @@ import backend from '../apis/backend';
 import { FETCH_USER } from './types';
 
 export const handleStripeToken = stripeToken => async dispatch => {
-  const { data: user } = await backend.post('/stripe', stripeToken);
+  const { data: user } = await backend.post('/api/stripe', stripeToken);
 
   dispatch({ type: FETCH_USER, payload: user });
 };
