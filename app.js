177f6ed8742f04a5762be1a9bdc86ec048a6747d
Behnam Ebrahimi <behnam.ebrahimi@gmail.com>
2020-01-10 15:53:25 +1100

    fixed a bug

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const app = express();
 
@@ -5,5 +6,17 @@ require('./startup/logging')();
 require('./startup/cors')(app);
 require('./startup/routes')(app);
 require('./startup/db')();
+
+if (process.env.NODE_ENV === 'production') {
+  // express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.static('client/build'));
+
+  // express will serve up the index.html file
+  // if it doesn't recognize the route
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 module.exports = app;
--- index.js
+++ index.js
@@ -1,18 +1,5 @@
 const app = require('./app');
 
-if (process.env.NODE_ENV === 'production') {
-  // express will serve up production assets
-  // like our main.js file, or main.css file!
-  app.use(express.static('client/build'));
-
-  // express will serve up the index.html file
-  // if it doesn't recognize the route
-  const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
-
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
