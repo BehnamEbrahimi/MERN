d9abfeeb81b8ff0c2f42dc949ef25272fbd545ff
Behnam Ebrahimi <behnam.ebrahimi@gmail.com>
2020-01-10 15:47:54 +1100

    -

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const app = require('./app');
 
 if (process.env.NODE_ENV === 'production') {
-  // Express will serve up production assets
+  // express will serve up production assets
   // like our main.js file, or main.css file!
   app.use(express.static('client/build'));
 
-  // Express will serve up the index.html file
+  // express will serve up the index.html file
   // if it doesn't recognize the route
   const path = require('path');
   app.get('*', (req, res) => {
